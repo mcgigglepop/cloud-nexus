16b812e6e05b454fe285fe863728f585f672f49f
cristopher ontiveros <christoph.alano@gmail.com>
2023-04-01 14:54:01 -0500

    added repository type and functions

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,8 +3,29 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/mcgigglepop/cloud-nexus/pkg/config"
 	"github.com/mcgigglepop/cloud-nexus/pkg/render"
 )
+
+// repository used by the handlers
+var Repo *Repository
+
+// repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
 
 // home
 func Home(w http.ResponseWriter, r *http.Request) {
