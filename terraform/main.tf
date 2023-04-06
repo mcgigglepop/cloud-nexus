4e94e6058c5b3d4d6123057cfdd18e1f7af4e692
cristopher ontiveros <christoph.alano@gmail.com>
2023-04-06 06:43:50 -0500

    added change to stage deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,18 @@ resource "aws_api_gateway_deployment" "example" {
   depends_on = [
     "aws_api_gateway_method.lambda_method", "aws_api_gateway_integration.lambda_integration"
   ]
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.api_gateway.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  variables = {
+    deployed_at = "timestampher"
+  }
 }
 
 resource "aws_api_gateway_stage" "example" {
@@ -155,4 +167,115 @@ resource "aws_lambda_permission" "lambda_permission" {
   function_name = "Test-function"
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api_gateway.id}/*/${aws_api_gateway_method.lambda_method.http_method}${aws_api_gateway_resource.lambda_resource.path}"
+}
+
+
+################################################
+
+# lambda resource endpoint
+resource "aws_api_gateway_resource" "lambda_resource2" {
+  path_part   = "${var.endpoint_path}-2"
+  parent_id   = aws_api_gateway_resource.lambda_resource_sub.id
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  
+}
+
+# lambda resource Method
+resource "aws_api_gateway_method" "lambda_method2" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.lambda_resource2.id
+  http_method   = var.http_method
+  authorization = "NONE"
+  api_key_required = true
+}
+
+# Method Integration
+resource "aws_api_gateway_integration" "lambda_integration2" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.lambda_resource2.id
+  http_method             = aws_api_gateway_method.lambda_method2.http_method
+  integration_http_method = var.http_method
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:621752543894:function:test2/invocations"
+  
+  request_parameters = {
+    "integration.request.header.X-Authorization" = "'static'"
+  }
+
+  # Transforms the incoming XML request to JSON
+  request_templates = {
+    "application/xml" = <<EOF
+{
+   "body" : $input.json('$')
+}
+EOF
+  }
+}
+
+# CORS OPTIONS Method for the lambda Endpoint
+resource "aws_api_gateway_method" "cors_method_lambda2" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.lambda_resource2.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# CORS Integration for the lambda Endpoint
+resource "aws_api_gateway_integration" "cors_integration_lambda2" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.lambda_resource2.id
+  http_method = aws_api_gateway_method.cors_method_lambda2.http_method
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = <<EOF
+{ "statusCode": 200 }
+EOF
+  }
+}
+
+# CORS Method Response for the lambda Endpoint
+resource "aws_api_gateway_method_response" "cors_method_response_lambda2" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_resource.lambda_resource2.id
+  http_method = aws_api_gateway_method.cors_method_lambda2.http_method
+
+  status_code = "200"
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+# CORS Integration Response for the lambda Endpoint
+resource "aws_api_gateway_integration_response" "cors_integration_response_lambda2" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  resource_id = aws_api_gateway_method.cors_method_lambda2.resource_id
+  http_method = aws_api_gateway_method.cors_method_lambda2.http_method
+
+  status_code = aws_api_gateway_method_response.cors_method_response_lambda2.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+
+
+
+
+
+# Permission to allow execution from api gateway to invoke the lambda function
+resource "aws_lambda_permission" "lambda_permission2" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "test2"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api_gateway.id}/*/${aws_api_gateway_method.lambda_method2.http_method}${aws_api_gateway_resource.lambda_resource2.path}"
 }
